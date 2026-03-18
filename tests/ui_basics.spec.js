df11798a48d8bc790949f126cbae2ff648cf18b7
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-03-18 15:57:23 +0530

    updated login test

--- tests/ui_basics.spec.js
+++ tests/ui_basics.spec.js
@@ -1,4 +1,4 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
 test("Browser context test", async ({ browser }) => {
   const context = await browser.newContext();
@@ -6,6 +6,21 @@ test("Browser context test", async ({ browser }) => {
   await page.goto("https://www.aicamp.ai/login");
 });
 
-test("Page playwright test", async ({ page }) => {
+test.only("Page playwright test", async ({ page }) => {
   await page.goto("https://www.aicamp.ai/login");
+  const title = await page.title();
+  console.log(title);
+  await expect(page).toHaveTitle(
+    "Learn AI with global developers community | AICamp",
+  );
+  await page
+    .getByRole("textbox", { name: "Email Address" })
+    .fill(process.env.TEST_USER || "");
+  await page.waitForTimeout(500);
+  await page
+    .getByRole("textbox", { name: "password or one time password" })
+    .fill(process.env.TEST_PSWD || "");
+  await page.waitForTimeout(500);
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.waitForEvent("load");
 });
